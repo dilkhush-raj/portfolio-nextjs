44d73e47fc5f8a3a0c9a4aa86771f949d511333a
Dilkhush Raj <dilkhush_raj@outlook.com>
2023-05-30 11:08:49 +0530

    Add suneditor

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import 'suneditor/dist/css/suneditor.min.css'; // Import Sun Editor's CSS File
 
 const inter = Inter({ subsets: ["latin"] });
 
